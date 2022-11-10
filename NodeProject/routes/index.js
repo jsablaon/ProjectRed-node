4023a9dd28693fe6a53fd5217bcef803716a8989
bestauffer <bestauffer1@gmail.com>
2022-11-10 15:00:10 -0800

    added delete button

--- NodeProject/routes/index.js
+++ NodeProject/routes/index.js
@@ -1,23 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
-giftArray = [
-  {name: "flowers1", price: "11"},
-  {name: "flowers2", price: "12"},
-  {name: "flowers3", price: "13"},
-  {name: "flowers4", price: "14"},
-];
+// giftArray = [
+//   {name: "flowers1", price: "11"},
+//   {name: "flowers2", price: "12"},
+//   {name: "flowers3", price: "13"},
+//   {name: "flowers4", price: "14"},
+// ];
 
-/* GET giftArray */
-router.get('/gifts', function(req, res, next) {
-  console.log(giftArray);
-  res.status(200).json(giftArray);
-});
+// /* GET giftArray */
+// router.get('/gifts', function(req, res, next) {
+//   console.log(giftArray);
+//   res.status(200).json(giftArray);
+// });
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
 
 
 
