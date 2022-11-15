dbeea819f5fb0ecb04c1801e4e7212f15e8a1a95
John <82200615+jsablaon@users.noreply.github.com>
2022-11-14 22:24:37 -0800

    Update userSchema.js
    update user schema to include userId, name, email

--- NodeProject/userSchema.js
+++ NodeProject/userSchema.js
@@ -7,19 +7,11 @@ const UserModel = new User({
         type: Number,
         required: true
     },
-    FName: {
+    Name: {
         type: String,
         required: true
-    },
-    LName: {
-        type: String,
-        required: true
-    },    
+    },   
     Email: {
-        type: String,
-        required: true
-    },    
-    Password: {
         type: String,
         required: true
     }
