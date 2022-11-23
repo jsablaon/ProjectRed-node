f1584f93435f1183c7e6f2e5b5f45387b5783d7d
bestauffer <77029920+bestauffer@users.noreply.github.com>
2022-11-22 18:20:59 -0800

    Update index.js

--- NodeProject/routes/index.js
+++ NodeProject/routes/index.js
@@ -4,33 +4,6 @@ var router = express.Router();
 
 const cartSchema = require('../cartSchema');
 
-// giftArray = [
-//   {name: "flowers1", price: "11"},
-//   {name: "flowers2", price: "12"},
-//   {name: "flowers3", price: "13"},
-//   {name: "flowers4", price: "14"},
-// ];
-
-// /* GET giftArray */
-// router.get('/gifts', function(req, res, next) {
-//   console.log(giftArray);
-//   res.status(200).json(giftArray);
-// });
-
-/* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
-
-
-
-// itemArray = [
-//   { userId: '123456789', storeId: 'storestringnumbers', itemId: '09876389', itemQty: 1, itemName: 'banana', itemPrice: 5.95, itemImage: 'https://www.w3schools.com/images/w3schools_green.jpg', itemVideo: '' },
-//   { userId: '102125659489827524664', storeId: '5668556', itemId: '098763', itemQty: 1, itemName: 'bread', itemPrice: 60.32, itemImage: 'https://www.w3schools.com/images/w3schools_green.jpg', itemVideo: '' },
-//   { userId: '123456789', storeId: '348674356845465', itemId: '5369465526', itemQty: 1, itemName: 'rum', itemPrice: 21.95, itemImage: 'https://www.w3schools.com/images/w3schools_green.jpg', itemVideo: '' }
-// ];
-
-
 /* GET items. */
 router.get('/items', function(req, res) {
   //console.log(itemArray);
@@ -53,25 +26,23 @@ router.get('/items', function(req, res) {
   // }, 50)
 
 
-
-  //res.status(200).json(itemArray);
 });
 
 /* GET item */
 router.get('/items/:id', function(req, res) {
-  let found = false;
-  for(var i=0; i < itemArray.length; i++)
-  {
-    if(itemArray[i].itemId == req.params.id)
-    {
-      console.log(itemArray[i]);
-      found = true;
-      res.status(200).json(itemArray[i]);
-    }
-  }
-  if(!found){
-    res.status(500).send("no such item");
-  }
+  // let found = false;
+  // for(var i=0; i < itemArray.length; i++)
+  // {
+  //   if(itemArray[i].itemId == req.params.id)
+  //   {
+  //     console.log(itemArray[i]);
+  //     found = true;
+  //     res.status(200).json(itemArray[i]);
+  //   }
+  // }
+  // if(!found){
+  //   res.status(500).send("no such item");
+  // }
 
 });
 
@@ -84,8 +55,8 @@ router.put('/items/:id', function(req, res) {
 
   setTimeout(async function() {
     let filter = {itemId: userId};
-    console.log(filter);
-    console.log(changedItem)
+    //console.log(filter);
+    //console.log(changedItem)
     let currentItem = await cartItemSchema.findOneAndUpdate({"userId": changedItem.userId, "itemId": changedItem.itemId}, changedItem);
     console.log(currentItem)
     try{
@@ -95,21 +66,6 @@ router.put('/items/:id', function(req, res) {
       res.status(400).json({ message: err.message })
     }   
   }, 1)
-  // var changedItem = new cartItemSchema(req.body);
-  // console.log(req.params.id);
-  // let found = false;
-  // for(var i=0; i < itemArray.length; i++)
-  // {
-  //   if(itemArray[i].itemId == req.params.id)
-  //   {
-  //     itemArray[i] = changedItem;
-  //     found = true;
-  //     res.status(200).json(itemArray[i]);
-  //   }
-  // }
-  // if(!found){
-  //   res.status(500).send("no such item");
-  // }
 });
 
 router.delete('/items/:id', function (req, res){
@@ -132,29 +88,12 @@ router.delete('/items/:id', function (req, res){
       }
     });
   }, 1)
-    // let found = false;
-    // for(var i=0; i < itemArray.length; i++)
-    // {
-    //   if( itemArray[i].itemId == req.params.id)
-    //   {
-    //     console.log(itemArray[i]);
-    //     found = true;
-    //     itemArray.splice(i,1);
-    //     res.status(200).send("delete successful");
-    //   }
-    // }
-    // if (found === false)
-    // {
-    //   res.status(500).send("no such item");
-    // }
 });
 
 router.post('/items', function(req,res){
   var newItem = new cartItemSchema(req.body);
   console.log(newItem);
-  //itemArray.push(newItem);
 
-  
   let found = false;
 
   setTimeout(async function() {
@@ -192,11 +131,6 @@ router.get('/cart', function(req, res) {
     }
     res.status(200).json(AllCarts);
   });
-  //res.status(200).json(itemArray);
 });
-
-hashCode = function(s){
-  return s.split("").reduce(function(a,b){a=((a<<5)-a)+b.charCodeAt(0);return a&a},0);
-}
 
 module.exports = router;
