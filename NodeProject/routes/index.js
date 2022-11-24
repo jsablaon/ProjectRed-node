2092520476879e7f5ecd9807d195b3e87fe9d87d
bcuser <blake.stauffer@bellevuecollege.edu>
2022-11-24 02:28:42 -0800

    fixed errors

--- NodeProject/routes/index.js
+++ NodeProject/routes/index.js
@@ -16,17 +16,6 @@ router.get('/items', function(req, res) {
     res.status(200).json(AllCartItems);
   });
 
-  // setTimeout(async function() {
-  //   // let filter = {itemId: userId};
-  //   // console.log(filter);
-  //   await cartItemSchema.find({}, (err, AllCartItems) => {
-  //   if(err){
-  //     res.status(500).send(err);
-  //   }
-  //   res.status(200).json(AllCartItems);
-  // });  
-  // }, 50)
-
 
 });
 
@@ -60,7 +49,7 @@ router.put('/items/:id', function(req, res) {
     //console.log(filter);
     //console.log(changedItem)
     let currentItem = await cartItemSchema.findOneAndUpdate({"userId": changedItem.userId, "itemId": changedItem.itemId}, changedItem);
-    console.log(currentItem)
+    //console.log(currentItem)
     try{
       res.status(200).json(currentItem)
     } 
@@ -86,43 +75,35 @@ router.put('/items/:id', function(req, res) {
   });
  });
 
-
-
-// router.delete('/items/:id', function (req, res){
-
-
-//   //var removedItem = new cartItemSchema(req.body);
-//   var removeId = req.params.id;
-//   //console.log(req.params.id);
-//   let found = false;
-
-//   setTimeout(async function() {
-//     let filter = {itemId: removeId};
-//     console.log(filter);
-//     await cartItemSchema.findOneAndDelete(filter, function(err) {
-//       if(!err){
-//         res.status(500).json("item deleted")
-//       }
-//       else{
-//         res.status(500).json("unable to delete item")
-//       }
-//     });
-//   }, 1)
-// });
-
 router.post('/items', function(req,res){
   var newItem = new cartItemSchema(req.body);
   console.log(newItem);
 
   let found = false;
 
+
+
+
   setTimeout(async function() {
-      try{
-        const newDocument = await newItem.save();
-        res.status(201).json(newDocument);
-      } catch(err) {
-        res.status(400).json({ message: err.message })
-      }
+    var items = await cartItemSchema.find({});
+      console.log(items);
+
+      const isFound = items.some(element => {
+        if (element.userId == newItem.userId && element.itemId == newItem.itemId){
+          return true;
+        }
+        return false;
+      });
+      //console.log(isFound)
+      if(!isFound)
+      {
+        try{
+          const newDocument = await newItem.save();
+          res.status(201).json(newDocument);
+        } catch(err) {
+          res.status(400).json({ message: err.message })
+        }
+      }    
     
     
   }, 10)
@@ -152,55 +133,5 @@ router.get('/cart', function(req, res) {
     res.status(200).json(AllCarts);
   });
 });
-
-router.post('/items', function(req,res){
-  var newItem = new cartItemSchema(req.body);
-  console.log(newItem);
-  itemArray.push(newItem);
-
-  
-  let found = false;
-
-  setTimeout(async function() {
-      try{
-        const newDocument = await newItem.save();
-        res.status(201).json(newDocument);
-      } catch(err) {
-        res.status(400).json({ message: err.message })
-      }
-    
-    
-  }, 10)
-
-});
-
-router.post('/cart', function(req,res){
-  var newCart = new cartSchema(req.body);
-
-  setTimeout(async function() {
-    try{
-      const newDocument = await newCart.save();
-      res.status(201).json(newDocument);
-    } catch(err) {
-      res.status(400).json({ message: err.message })
-    }
-  }, 10)
-});
-
-/* GET carts. */
-router.get('/cart', function(req, res) {
-  //console.log(itemArray);
-  cartSchema.find({}, (err, AllCarts) => {
-    if(err){
-      res.status(500).send(err);
-    }
-    res.status(200).json(AllCarts);
-  });
-  //res.status(200).json(itemArray);
-});
-
-hashCode = function(s){
-  return s.split("").reduce(function(a,b){a=((a<<5)-a)+b.charCodeAt(0);return a&a},0);
-}
 
 module.exports = router;
