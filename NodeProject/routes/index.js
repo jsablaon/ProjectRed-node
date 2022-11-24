5b038777ef1615c2b39ae21ef0935f96f6cf1b40
bestauffer <77029920+bestauffer@users.noreply.github.com>
2022-11-24 02:41:25 -0800

    Update index.js

--- NodeProject/routes/index.js
+++ NodeProject/routes/index.js
@@ -21,19 +21,7 @@ router.get('/items', function(req, res) {
 
 /* GET item */
 router.get('/items/:id', function(req, res) {
-  // let found = false;
-  // for(var i=0; i < itemArray.length; i++)
-  // {
-  //   if(itemArray[i].itemId == req.params.id)
-  //   {
-  //     console.log(itemArray[i]);
-  //     found = true;
-  //     res.status(200).json(itemArray[i]);
-  //   }
-  // }
-  // if(!found){
-  //   res.status(500).send("no such item");
-  // }
+  
 
 });
 
@@ -41,15 +29,10 @@ router.get('/items/:id', function(req, res) {
 router.put('/items/:id', function(req, res) {
   var changedItem = new cartItemSchema(req.body);
   userId = req.params.id;
-  //console.log(req.params.id);
-  let found = false;
 
   setTimeout(async function() {
-    let filter = {itemId: userId};
-    //console.log(filter);
-    //console.log(changedItem)
     let currentItem = await cartItemSchema.findOneAndUpdate({"userId": changedItem.userId, "itemId": changedItem.itemId}, changedItem);
-    //console.log(currentItem)
+
     try{
       res.status(200).json(currentItem)
     } 
@@ -59,12 +42,12 @@ router.put('/items/:id', function(req, res) {
   }, 1)
 });
 
-
+//delete item
  router.delete('/deleteitems', async (req, res) => {
   let pUserId = url.parse(req.url,true).query['userId'];
   let pItemId = url.parse(req.url,true).query['itemId'];
   let pItemQty = url.parse(req.url,true).query['itemQty'];
-  //console.log(pUserId);
+
   await cartItemSchema.findOneAndDelete({userId: pUserId, itemId: pItemId, itemQty: pItemQty}, function(err) {
     if(!err){
         res.status(500).json("item deleted")
@@ -75,26 +58,20 @@ router.put('/items/:id', function(req, res) {
   });
  });
 
+ //Add item to cart
 router.post('/items', function(req,res){
   var newItem = new cartItemSchema(req.body);
   console.log(newItem);
 
-  let found = false;
-
-
-
-
   setTimeout(async function() {
     var items = await cartItemSchema.find({});
-      console.log(items);
-
       const isFound = items.some(element => {
         if (element.userId == newItem.userId && element.itemId == newItem.itemId){
           return true;
         }
         return false;
       });
-      //console.log(isFound)
+  
       if(!isFound)
       {
         try{
@@ -104,12 +81,11 @@ router.post('/items', function(req,res){
           res.status(400).json({ message: err.message })
         }
       }    
-    
-    
   }, 10)
 
 });
 
+//Add cart
 router.post('/cart', function(req,res){
   var newCart = new cartSchema(req.body);
 
@@ -125,7 +101,6 @@ router.post('/cart', function(req,res){
 
 /* GET carts. */
 router.get('/cart', function(req, res) {
-  //console.log(itemArray);
   cartSchema.find({}, (err, AllCarts) => {
     if(err){
       res.status(500).send(err);
